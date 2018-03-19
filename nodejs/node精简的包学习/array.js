dab2eb54c55d2f68a937b08df0ad29e97d954c2c
michaelcheng <422201870@qq.com>
2018-03-19 15:20:55 +0800

    每日一练

--- nodejs/node精简的包学习/array.js
+++ nodejs/node精简的包学习/array.js
@@ -83,3 +83,78 @@ module.exports = function arrayLast(arr, num) {
 	}
 	return res;
 };
+
+// 5. flatten 数组中的元素，[1,2,3,[2,3],4] => [1,2,3,2,3,4]
+module.exports = function (arr) {
+	return flat(arr, []);
+}
+
+function flat (arr, res) {
+	var i=0, cur;
+	var length = arr.length;
+	for(; i++ ;i<length){
+		cur=arr[i];
+		Array.isArray(cur) ? flat(cur, res) : res.push(arr[i]);
+	}
+	return res;
+}
+
+// 6. 根据指定规则指定数组的数组，支持传一个参数或者两个参数.
+module.exports = function newArray(start, end) {
+	var n0 = typeof start === 'number',
+			n1 = typeof end === 'number'
+
+	if (n0 && !n1) {
+			end = start
+			start = 0
+	} else if (!n0 && !n1) {
+			start = 0
+			end = 0
+	}
+
+	start = start|0
+	end = end|0
+	var len = end-start
+	if (len<0)
+			throw new Error('array length must be positive')
+	
+	var a = new Array(len)
+	for (var i=0, c=start; i<len; i++, c++)
+			a[i] = c
+	return a
+}
+
+// 7. 数组去重,思路是利用对象不能有重复的key的特性来处理，client表示数据源，hasher表示对数据的特殊规则
+module.exports = function deputed(client, hasher) {
+	hasher = hasher || JSON.stringify
+
+	let res = [];
+	let lookup = {};
+
+	for(i = 0; i < client.length; i++){
+		let ele = client[i];
+		let eleHash = hasher(ele);
+		if(!lookup[eleHash]){
+			res.push(eleHash);
+			lookup[eleHash] = true;
+		}
+	}
+	return res;
+}
+
+// 8. 根据指定规则生成数组数据
+module.exports = function (item, n) {
+	var ret = new Array(n);
+	var isFn = typeof item === 'function';
+
+	// ensure fill method
+	if (!isFn && typeof ret.fill === 'function') {
+		return ret.fill(item);
+	}
+
+	for (var i = 0; i < n; i++) {
+		ret[i] = isFn ? item(i, n, ret) : item;
+	}
+
+	return ret;
+};
