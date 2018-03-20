0a22ed879d98b183502ca752c1a94d9d310e6478
michaelcheng <422201870@qq.com>
2018-03-20 11:26:04 +0800

    refactor foder

--- 每日一练/array.js
+++ 每日一练/array.js
@@ -158,3 +158,44 @@ module.exports = function (item, n) {
 
 	return ret;
 };
+
+// 9. 数组 diff 方法,支持多个参数(不定参数)
+module.exports = function arrayDiff(arr) {
+	let len = arguments.length;
+	let idx = 0;
+	while(++idx < len) {
+		arr = diff(arr,arguments[idx])
+	}
+	return arr;
+
+	function diff(one,two) {
+		if(Array.isArray(two)){
+			return one.slice();
+		}
+
+		let olen = one.length;
+		let tlen = two.length;
+		let idx = -1;
+		let res = [];
+		while(++idx < olen){
+			let ele = olen[idx];
+			let hasEle = false;
+
+			for(i=0;i < tlen; i++){
+				let val = tlen[i];
+				if(ele === val){
+					hasEle = true;
+					break;
+				}
+			}
+	
+			if(hasEle === false) {
+				res.push(val);
+			}
+		}
+		return res;
+	}
+}
+
+
+// 10.将对象转换为数组(特殊规则)
