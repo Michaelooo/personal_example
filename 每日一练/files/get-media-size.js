4f69e394018186ce1f397c05d8e54b881f254624
michaelcheng <422201870@qq.com>
2018-04-09 15:22:08 +0800

    每日一练

---
+++ 每日一练/files/get-media-size.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * getMediaSize
+ * @param  {image|video|canvas|context} media The element to read the size from
+ * @param  {number} scale                     Optional. Convenience feature to transform the size if you're using retina canvas, for example
+ * @return {object}                           Contains width and height of the passed media
+ */
+
+function getMediaSize(media, scale) {
+	if (!media) {
+		return {
+			width: 0,
+			height: 0
+		};
+	}
+
+	if (media.canvas) { // it's a ctx
+		media = media.canvas;
+	}
+
+	const size = {
+		width: media.getContext ? media.width : media.naturalWidth || media.videoWidth || 0,
+		height: media.getContext ? media.height : media.naturalHeight || media.videoHeight || 0
+	};
+
+	if (scale) {
+		size.width /= scale;  // 除赋值
+		size.height /= scale;
+	}
+
+	return size;
+}
+
+export default function getMediaSizeAsync(media, scale) {
+	if (!media) {
+		return Promise.reject({
+			width: 0,
+			height: 0
+		});
+	}
+	return new Promise(resolve => {
+		const instantSize = getMediaSize(media, scale);
+		if (instantSize.width) {
+			return resolve(instantSize);
+		}
+		if (media.tagName === 'VIDEO') {
+			return media.addEventListener('loadedmetadata', () => resolve(getMediaSize(media, scale)));
+		}
+		setTimeout(function check() {
+			const size = getMediaSize(media, scale);
+			if (size.width) {
+				resolve(size);
+			} else {
+				setTimeout(check, 100);
+			}
+		}, 100);
+	});
+}
+
+getMediaSizeAsync.sync = getMediaSize;
---
+++ 每日一练/semver/semver-max.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// semver 是一个语义化的库, https://github.com/npm/node-semver
+
+var semver = require('semver');
+
+
+var makeMax2 = function (op) {
+  return function (v1, v2) {
+    return semver[op](v2.clean, v1.clean) ? v2 : v1;
+  };
+};
+
+
+var makeMax = function (op) {
+  return function () {
+    return []
+      .filter.call(arguments, function (version) {
+        return semver.valid(version, true);
+      })
+      .map(function (version) {
+        return {
+          original: version,
+          clean: semver.clean(version)
+        };
+      })
+      .reduce(makeMax2(op))
+      .original;
+  };
+};
+
+
+module.exports = (function (max) {
+  max.gt = max;
+
+  ['gte', 'lt', 'lte'].forEach(function (op) {
+    max[op] = makeMax(op);
+  });
+
+  return max;
+}(makeMax('gt')));
