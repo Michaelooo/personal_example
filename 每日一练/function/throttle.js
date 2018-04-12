47c494f30f991a202130a82fa089e0934a1c11f7
michaelcheng <422201870@qq.com>
2018-04-12 17:47:34 +0800

    每日一练

---
+++ 每日一练/function/throttle.js
@@ -0,0 +1,141 @@
+
+// 关于 throttle 和 debounce 函数，前者是控制函数固定的频率和出发次数，从而降低频繁事情回调的执行次数。 后者是在某段时间内，多次时间触犯只执行一次回调函数，比如resize 方法
+/**
+ * Throttle execution of a function. Especially useful for rate limiting
+ * execution of handlers on events like resize and scroll.
+ *
+ * @param  {Number}    delay          A zero-or-greater delay in milliseconds. For event callbacks, values around 100 or 250 (or even higher) are most useful.
+ * @param  {Boolean}   noTrailing     Optional, defaults to false. If noTrailing is true, callback will only execute every `delay` milliseconds while the
+ *                                    throttled-function is being called. If noTrailing is false or unspecified, callback will be executed one final time
+ *                                    after the last throttled-function call. (After the throttled-function has not been called for `delay` milliseconds,
+ *                                    the internal counter is reset)
+ * @param  {Function}  callback       A function to be executed after delay milliseconds. The `this` context and all arguments are passed through, as-is,
+ *                                    to `callback` when the throttled-function is executed.
+ * @param  {Boolean}   debounceMode   If `debounceMode` is true (at begin), schedule `clear` to execute after `delay` ms. If `debounceMode` is false (at end),
+ *                                    schedule `callback` to execute after `delay` ms.
+ *
+ * @return {Function}  A new, throttled, function.
+ */
+function throttle( delay, noTrailing, callback, debounceMode ) {
+
+	// After wrapper has stopped being called, this timeout ensures that
+	// `callback` is executed at the proper times in `throttle` and `end`
+	// debounce modes.
+	var timeoutID;
+
+	// Keep track of the last time `callback` was executed.
+	var lastExec = 0;
+
+	// `noTrailing` defaults to falsy.
+	if ( typeof noTrailing !== 'boolean' ) {
+		debounceMode = callback;
+		callback = noTrailing;
+		noTrailing = undefined;
+	}
+
+	// The `wrapper` function encapsulates all of the throttling / debouncing
+	// functionality and when executed will limit the rate at which `callback`
+	// is executed.
+	function wrapper () {
+
+		var self = this;
+		var elapsed = Number(new Date()) - lastExec;
+		var args = arguments;
+
+		// Execute `callback` and update the `lastExec` timestamp.
+		function exec () {
+			lastExec = Number(new Date());
+			callback.apply(self, args);
+		}
+
+		// If `debounceMode` is true (at begin) this is used to clear the flag
+		// to allow future `callback` executions.
+		function clear () {
+			timeoutID = undefined;
+		}
+
+		if ( debounceMode && !timeoutID ) {
+			// Since `wrapper` is being called for the first time and
+			// `debounceMode` is true (at begin), execute `callback`.
+			exec();
+		}
+
+		// Clear any existing timeout.
+		if ( timeoutID ) {
+			clearTimeout(timeoutID);
+		}
+
+		if ( debounceMode === undefined && elapsed > delay ) {
+			// In throttle mode, if `delay` time has been exceeded, execute
+			// `callback`.
+			exec();
+
+		} else if ( noTrailing !== true ) {
+			// In trailing throttle mode, since `delay` time has not been
+			// exceeded, schedule `callback` to execute `delay` ms after most
+			// recent execution.
+			//
+			// If `debounceMode` is true (at begin), schedule `clear` to execute
+			// after `delay` ms.
+			//
+			// If `debounceMode` is false (at end), schedule `callback` to
+			// execute after `delay` ms.
+			timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
+		}
+
+	}
+
+	// Return the wrapper function.
+	return wrapper;
+
+}
+
+
+
+// 自己实现的，上面的简化版
+/**
+*
+* @param fn {Function}   实际要执行的函数
+* @param delay {Number}  执行间隔，单位是毫秒（ms）
+*
+* @return {Function}     返回一个“节流”函数
+*/
+
+function throttle(fn, threshhold) {
+
+  // 记录上次执行的时间
+  var last
+
+  // 定时器
+  var timer
+
+  // 默认间隔为 250ms
+  threshhold || (threshhold = 250)
+
+  // 返回的函数，每过 threshhold 毫秒就执行一次 fn 函数
+  return function () {
+
+    // 保存函数调用时的上下文和参数，传递给 fn
+    var context = this
+    var args = arguments
+
+    var now = +new Date()
+
+    // 如果距离上次执行 fn 函数的时间小于 threshhold，那么就放弃
+    // 执行 fn，并重新计时
+    if (last && now < last + threshhold) {
+      clearTimeout(timer)
+
+      // 保证在当前时间区间结束后，再执行一次 fn
+      timer = setTimeout(function () {
+        last = now
+        fn.apply(context, args)
+      }, threshhold)
+
+    // 在时间区间的最开始和到达指定间隔的时候执行一次 fn
+    } else {
+      last = now
+      fn.apply(context, args)
+    }
+  }
+}
