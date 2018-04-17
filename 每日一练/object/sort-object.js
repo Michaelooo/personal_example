e5742a8805ea9768f9090ccf8ba3bdf6b716e68b
michaelcheng <422201870@qq.com>
2018-04-17 17:57:22 +0800

    每日一练

---
+++ 每日一练/object/sort-object.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(input) {
+	var output = {};
+
+	Object.keys(input)
+		.sort()
+		.forEach(function(key) {
+			output[key] = input[key];
+		});
+
+	return output;
+};
